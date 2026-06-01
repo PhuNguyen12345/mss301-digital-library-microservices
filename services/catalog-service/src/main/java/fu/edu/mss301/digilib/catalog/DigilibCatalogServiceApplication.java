396d1ffe74d061453afe73ef4b086a08d7379d0e
PhuNguyen12345 <phukak12345@gmail.com>
2026-06-01 17:10:47 +0700

    feat: update catalog

---
+++ services/catalog-service/src/main/java/fu/edu/mss301/digilib/catalog/DigilibCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package fu.edu.mss301.digilib.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigilibCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigilibCatalogServiceApplication.class, args);
+	}
+
+}
