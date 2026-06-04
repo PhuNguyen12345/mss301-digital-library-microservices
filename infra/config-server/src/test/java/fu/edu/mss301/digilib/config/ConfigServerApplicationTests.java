84bf2ef3d56596cd80f25718df59b5cdb00fe342
PhuNguyen12345 <phukak12345@gmail.com>
2026-06-04 22:09:02 +0700

    feat: update config-server

---
+++ infra/config-server/src/test/java/fu/edu/mss301/digilib/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fu.edu.mss301.digilib.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
